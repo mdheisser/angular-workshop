21011df099a3813053e81a752b9ee2670ac9ecfb
TopTalentDev <toptal715@gmail.com>
2019-03-02 20:12:59 -0300

    app(core) remove unnecessary things

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,7 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { RouteReuseStrategy, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-import { RouteReusableStrategy } from './route-reusable-strategy';
-import { HttpService } from './http/http.service';
-import { HttpCacheService } from './http/http-cache.service';
-import { ApiPrefixInterceptor } from './http/api-prefix.interceptor';
-import { ErrorHandlerInterceptor } from './http/error-handler.interceptor';
-import { CacheInterceptor } from './http/cache.interceptor';
 /* our own custom services  */
 // e.g:
 // import { ContactService } from '@app/core/contact.service';
@@ -18,23 +11,10 @@ import { CacheInterceptor } from './http/cache.interceptor';
   imports: [
     CommonModule,
     HttpClientModule,
-    RouterModule
   ],
   declarations: [],
   providers: [
     // ContactService
-    HttpCacheService,
-    ApiPrefixInterceptor,
-    ErrorHandlerInterceptor,
-    CacheInterceptor,
-    {
-      provide: HttpClient,
-      useClass: HttpService
-    },
-    {
-      provide: RouteReuseStrategy,
-      useClass: RouteReusableStrategy
-    }
   ]
 })
 
